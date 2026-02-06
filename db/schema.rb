6e95716c3d0d2117e819c460f523bd8a91bbe957
sharoa119 <sau.rin1109@gmail.com>
2026-02-06 15:09:12 +0900

    Add HomeController and root page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_02_04_012837) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_06_034151) do
+  create_table "medication_records", force: :cascade do |t|
+    t.integer "medication_time_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["medication_time_id"], name: "index_medication_records_on_medication_time_id"
+  end
+
+  create_table "medication_schedules", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_medication_schedules_on_user_id"
+  end
+
+  create_table "medication_times", force: :cascade do |t|
+    t.integer "medication_schedule_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["medication_schedule_id"], name: "index_medication_times_on_medication_schedule_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "line_user_id", null: false
     t.datetime "created_at", null: false
@@ -18,4 +39,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_04_012837) do
     t.index ["line_user_id"], name: "index_users_on_line_user_id", unique: true
   end
 
+  add_foreign_key "medication_records", "medication_times"
+  add_foreign_key "medication_schedules", "users"
+  add_foreign_key "medication_times", "medication_schedules"
 end
