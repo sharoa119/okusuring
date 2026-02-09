f6368d000732697442931e7d5bdbab41b5638ac7
sharoa119 <sau.rin1109@gmail.com>
2026-02-09 13:49:28 +0900

    Render today's medication schedules with dummy data

--- app/controllers/medication_schedules_controller.rb
+++ app/controllers/medication_schedules_controller.rb
@@ -1,5 +1,23 @@
 class MedicationSchedulesController < ApplicationController
   def index
-    @medication_schedules = MedicationSchedule.all
+    @today_schedules = [
+      {
+        name: "自分",
+        schedules: [
+          { time: "08:00", name: "朝の薬" },
+          { time: "20:00", name: "夜の薬" }
+        ]
+      },
+      {
+        name: "line_user_id",
+        schedules: []
+      },
+      {
+        name: "子ども",
+        schedules: [
+          { time: "07:30", name: "朝の薬" }
+        ]
+      }
+    ]
   end
 end
