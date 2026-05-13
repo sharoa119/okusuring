93eef6992983ebfccc9acecb564c1d298a0d3edf
sharoa119 <sau.rin1109@gmail.com>
2026-05-13 15:25:10 +0900

    LINE webhook受信とユーザー紐付けを実装

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -4,8 +4,19 @@ class LineBotController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def callback
-    Rails.logger.info("=== LINE WEBHOOK ===")
-    Rails.logger.info(params)
+    event = params[:events].first
+    user_id = event.dig("source", "userId")
+
+    Rails.logger.info("LINE user_id: #{user_id}")
+    Rails.logger.info(params.to_unsafe_h)
+
+    user = User.find_by(line_user_id: user_id)
+  
+    if user
+      user.update!(line_bot_connected: true)
+    else
+      Rails.logger.info("Unlinked LINE user (ignored): #{user_id}")
+    end
 
     render plain: "OK"
   end
