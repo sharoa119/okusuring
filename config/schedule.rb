adafaa21a1e08bdead46f486df940043222e4326
sharoa119 <sau.rin1109@gmail.com>
2026-05-21 14:49:11 +0900

    LINE通知をcronで自動実行

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,8 @@
 set :environment, "development"
+env :PATH, ENV["PATH"]
+set :output, "log/cron.log"
+
+job_type :runner, "cd :path && /Users/saully/.rbenv/shims/bundle exec rails runner -e :environment ':task' :output"
 
 every 1.minute do
   runner "MedicationNotifier.notify_now"
