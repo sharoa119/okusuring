a2b0eb92ef06dd16740839a836fa11e3eece7e74
sharoa119 <sau.rin1109@gmail.com>
2026-06-22 10:43:39 +0900

    通知設定と既存ページのテストを整備

--- test/controllers/medication_records_controller_test.rb
+++ test/controllers/medication_records_controller_test.rb
@@ -1,8 +1,6 @@
+# frozen_string_literal: true
+
 require "test_helper"
 
 class MedicationRecordsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get medication_records_create_url
-    assert_response :success
-  end
 end
--- test/controllers/notification_settings_controller_test.rb
+++ test/controllers/notification_settings_controller_test.rb
@@ -1,8 +1,38 @@
+# frozen_string_literal: true
+
 require "test_helper"
 
 class NotificationSettingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get notification_settings_show_url
+  setup do
+    OmniAuth.config.test_mode = true
+  end
+
+  teardown do
+    OmniAuth.config.test_mode = false
+    OmniAuth.config.mock_auth[:line] = nil
+  end
+
+  test "ログイン済みの場合は通知設定ページが表示される" do
+    user = users(:owner)
+
+    log_in_as(user)
+
+    get notification_settings_url
+
     assert_response :success
+    assert_match "通知設定", response.body
+    assert_match "再通知", response.body
+  end
+
+  private
+
+  def log_in_as(user)
+    OmniAuth.config.mock_auth[:line] = OmniAuth::AuthHash.new(
+      provider: "line",
+      uid: user.line_user_id,
+      info: { name: user.name }
+    )
+
+    get "/auth/line/callback"
   end
 end
