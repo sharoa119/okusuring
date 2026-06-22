6802f8d3309a89abbfb6a38ae45e89c18966d3f9
sharoa119 <sau.rin1109@gmail.com>
2026-06-22 10:30:56 +0900

    服薬スケジュールのコントローラテストを追加

--- test/controllers/medication_schedules_controller_test.rb
+++ test/controllers/medication_schedules_controller_test.rb
@@ -47,6 +47,82 @@ class MedicationSchedulesControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_path
   end
 
+  test "自分のお薬の予定を閲覧できる" do
+    user = users(:owner)
+    schedule = medication_schedules(:owner_schedule)
+
+    log_in_as(user)
+
+    get medication_schedule_path(schedule)
+
+    assert_response :success
+    assert_match "血圧の薬", response.body
+  end
+
+  test "家族共有されたお薬の予定を閲覧できる" do
+    user = users(:member)
+    schedule = medication_schedules(:owner_schedule)
+
+    log_in_as(user)
+
+    get medication_schedule_path(schedule)
+
+    assert_response :success
+    assert_match "血圧の薬", response.body
+    assert_match "閲覧専用です", response.body
+  end
+
+    test "家族共有されていないお薬の予定は閲覧できない" do
+      user = users(:not_family_member)
+      schedule = medication_schedules(:owner_schedule)
+
+      log_in_as(user)
+
+      get medication_schedule_path(schedule)
+
+      assert_redirected_to root_path
+    end
+
+  test "自分のお薬の予定を編集して更新できる" do
+    user = users(:owner)
+    schedule = medication_schedules(:owner_schedule)
+
+    log_in_as(user)
+
+    patch medication_schedule_path(schedule), params: {
+      medication_schedule: {
+        title: "血圧の薬（変更後）",
+        target_name: "自分",
+        memo: "夕食後に飲む",
+        medication_times_attributes: {
+          "0" => {
+            id: medication_times(:morning_time).id,
+            time: "2025-01-01 10:00:00"
+          }
+        }
+      }
+    }
+
+    assert_redirected_to root_path
+
+    schedule.reload
+    assert_equal "血圧の薬（変更後）", schedule.title
+    assert_equal "夕食後に飲む", schedule.memo
+  end
+
+  test "自分のお薬の予定を削除できる" do
+    user = users(:owner)
+    schedule = medication_schedules(:owner_schedule)
+
+    log_in_as(user)
+
+    assert_difference("MedicationSchedule.count", -1) do
+      delete medication_schedule_path(schedule)
+    end
+
+    assert_redirected_to root_path
+  end
+
   private
 
   def log_in_as(user)
