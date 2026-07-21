62c4c8395280b7fcc08cdd61ccd541d3d4dfda50
sharoa119 <sau.rin1109@gmail.com>
2026-07-21 17:26:25 +0900

    服薬記録の存在判定をexists?に変更

--- app/models/medication_time.rb
+++ app/models/medication_time.rb
@@ -5,6 +5,6 @@ class MedicationTime < ApplicationRecord
   has_many :medication_records, dependent: :destroy
 
   def taken_today?
-    medication_records.any? { |r| r.taken_date == Date.current }
+    medication_records.exists?(taken_date: Date.current)
   end
 end
